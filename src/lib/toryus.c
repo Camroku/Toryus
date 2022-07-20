251fef5d2e34599de91724ac50bde4a962b7d822
Camroku <cinaryilmaz.gnu@gmail.com>
2022-07-21 00:09:47 +0300

    delete unnecessary code

--- src/lib/toryus.c
+++ src/lib/toryus.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <terminal.h>
 
-extern void panic(const char *message, const char *file, uint32_t line)
+void panic(const char *message, const char *file, uint32_t line)
 {
     // We encountered a massive problem and have to stop.
     asm volatile("cli"); // Disable interrupts.
@@ -18,7 +18,7 @@ extern void panic(const char *message, const char *file, uint32_t line)
     for(;;);
 }
 
-extern void panic_assert(const char *file, uint32_t line, const char *desc)
+void panic_assert(const char *file, uint32_t line, const char *desc)
 {
     // An assertion failed, and we have to panic.
     asm volatile("cli"); // Disable interrupts.
